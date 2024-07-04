9603382e194f46d71ab5a21e49f319619b6c1a73
孟祥龙 <mxl688721@163.com>
2024-07-04 15:30:37 +0800

    settings first

--- day06/settings.py
+++ day06/settings.py
@@ -75,16 +75,6 @@ WSGI_APPLICATION = 'day06.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'day06',
-        'USER': 'root',
-        "PASSWORD": '668034ky',
-        'HOST': '127.0.0.1',
-        'POST': 3306,
-    }
-}
 
 
 # Password validation
@@ -231,5 +221,5 @@ MESSAGE_TAGS = {
 
 try:
     from .local_settings import *
-except Exception:
+except ImportError:
     pass
